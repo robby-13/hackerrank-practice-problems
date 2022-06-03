362e4a225a946fbdd8d058789de05230d6527a86
Robby Singh <73762993+robby-13@users.noreply.github.com>
2022-06-03 11:13:38 -0500

    Update timeConversion.py

--- timeConversion.py
+++ timeConversion.py
@@ -9,32 +9,25 @@ Example:
       Return '00:01:00'
 
 '''
-
 def timeConversion(s):
     # Write your code here
-    # first check whether the time is AM or PM
+    # first check whether the time is AM or PM and then strip the AM/PM substrings
     if s[-2:] == 'PM':
-        # check if it is a noon time
-        if s[:2] == '12':
-            s = s.replace('PM', '')
-        # for all other times, two things need to be done
-        # 1.) strip the PM
-        # 2.) increment the time by 12
-        else:
-            s = s.replace('PM', '')
+        s = s.replace('PM', '')
+        # noon times just need the PM stripped
+        # for all other times, the hour must be incremented by 12
+        if s[:2] != '12':
             hour = int(s[:2])
             newHour = str(hour + 12)
             s = s.replace(s[:2], newHour)
     else:
-        # check if midnight time:
+        # same procedure but swapped 
+        s = s.replace('AM', '')
         if s[:2] == '12':
             s = s.replace('AM', '')
             hour = int(s[:2])
             newHour = '00'
             s = s.replace(s[:2], newHour)
-        # else, just need to strip the AM 
-        else:
-            s = s.replace('AM', '')
             
             
     return s
